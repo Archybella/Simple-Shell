7cb7e5c46f1654b32fcdd81188d3f78bda30f870
Archybella <128797928+Archybella@users.noreply.github.com>
2023-07-22 04:55:49 +0300

    Update shell.h
    drjr

--- shell.h
+++ shell.h
@@ -112,109 +112,121 @@ typedef struct builtin
 
 
 /* toem_is_atoi.c */
-int toem_is_delim(char, char *);
-int toem_isalpha(int);
-int toem_atoi(char *);
+int is_delim(char, char *);
+int isalpha(int);
+int is_atoi(char *);
 
 /* toem_builtin_extra.c */
-int toem_handle_history(info_t *);
-int toem_handle_alias(info_t *);
+int handle_history(info_t *);
+int unset_alias(info_t *);
+int print_alias(info_t *);
+int set_alias(info_t *);
+int handle_alias(info_t *);
 
 /* toem_inbuilt.c */
-int toem_handle_exit(info_t *);
-int toem_handle_cd(info_t *);
-int toem_handle_help(info_t *);
+int handle_exit(info_t *);
+int handle_cd(info_t *);
+int handle_help(info_t *);
 
 /* toem_environment.c */
-char *toem_getenv(info_t *, const char *);
-int toem_handle_env(info_t *);
-int toem_handle_setenv(info_t *);
-int toem_handle_unsetenv(info_t *);
-int toem_populate_env_list(info_t *);
+char *_custom_getenv(info_t *, const char *);
+int _custom_handle_env(info_t *);
+int _custom_handle_setenv(info_t *);
+int _custom_handle_unsetenv(info_t *);
+int _custom_populate_env_list(info_t *);
 
 /* toem_psb_errors.c */
-void toem_eputs(char *);
-int toem_eputchar(char);
-int toem_putfd(char c, int fd);
-int toem_putsfd(char *str, int fd);
+void _custom_eputs(char *);
+int _custom_eputchar(char);
+int _custom_putfd(char c, int fd);
+int _custom_putsfd(char *str, int fd);
 
 /* toem_psb_errors1.c */
-int toem_erratoi(char *);
-void toem_print_error(info_t *, char *);
-int toem_print_d(int, int);
-char *toem_convert_number(long int, int, int);
-void toem_remove_comments(char *);
+int _custom_erratoi(char *);
+void custom_print_error(info_t *, char *);
+int custom_print_d(int, int);
+char *custom_convert_number(long int, int, int);
 
 /* toem_psb_exits.c */
-char *toem_strncpy(char *, char *, int);
-char *toem_strncat(char *, char *, int);
-char *toem_strchr(char *, char);
+char *custom_strncpy(char *, char *, int);
+char *custom_strncat(char *, char *, int);
+char *custom_strchr(char *, char);
 
 /* toem_get_line.c */
-ssize_t toem_get_input(info_t *);
-int toem_getline(info_t *, char **, size_t *);
-void toem_sigintHandler(int);
+ssize_t custom_get_input(info_t *);
+int custom_getline(info_t *, char **, size_t *);
+void custom_sigintHandler(int);
 
 /* toem_get_information.c */
-void toem_clear_info(info_t *);
-void toem_set_info(info_t *, char **);
-void toem_free_info(info_t *, int);
+void custom_clear_info(info_t *);
+void custom_set_info(info_t *, char **);
+void custom_free_info(info_t *, int);
 
 /* toem_hstry.c */
-int toem_write_history(info_t *info);
-int toem_read_history(info_t *info);
-int toem_build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
+int get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
 
 /* toem_lst.c */
-list_t *toem_add_node(list_t **, const char *, int);
-list_t *toem_add_node_end(list_t **, const char *, int);
-size_t toem_print_list_str(const list_t *);
-int toem_delete_node_at_index(list_t **, unsigned int);
-void toem_free_list(list_t **);
+list_t *new_add_node(list_t **, const char *, int);
+list_t *new_add_node_end(list_t **, const char *, int);
+size_t print_new_list_str(const list_t *);
+int delete_new_node_at_index(list_t **, unsigned int);
+void free_new_list(list_t **);
 
 /* toem_lst_1.c */
-size_t toem_list_len(const list_t *);
-char **toem_list_to_strings(list_t *);
-size_t toem_print_list(const list_t *);
-list_t *toem_node_starts_with(list_t *, char *, char);
-ssize_t toem_get_node_index(list_t *, list_t *);
+size_t new_list_len(const list_t *);
+char **list_to_new_strings(list_t *);
+size_t print_new_list(const list_t *);
+list_t *new_node_starts_with(list_t *, char *, char);
+ssize_t get_new_node_index(list_t *, list_t *);
 
 /* toem_mem_mangmnt.c */
-int toem_bfree(void **);
+int free_and_null(void **);
 
 /* toem_chk_dub_exe.c */
-int toem_is_chain(info_t *, char *, size_t *);
-void toem_check_chain(info_t *, char *, size_t *, size_t, size_t);
+int check_cmd(info_t *, char *);
+char *duplicate_chars(char *, int, int);
+char *find_executable(info_t *, char *, char *);
 
 /* toem_mem_realloc.c */
-char *toem_memset(char *, char, unsigned int);
-void toem_ffree(char **);
-void *toem_realloc(void *, unsigned int, unsigned int);
+char *custom_memset(char *, char, unsigned int);
+void free_string_array(char **);
+void *custom_realloc(void *, unsigned int, unsigned int);
 
 /* toem_shell_condition.c */
-int toem_loophsh(char **);
+int is_interactive_mode(char **);
+int find_builtin_command(info_t *);
+int fork_command(info_t *);
+int find_command_in_path(info_t *, char *, char *);
+int hsh(info_t *, char **);
 
 /* toem_strn_len.c */
-int toem_strlen(char *);
-int toem_strcmp(char *, char *);
-char *toem_starts_with(const char *, const char *);
-char *toem_strcat(char *, char *);
+int custom_strlen(char *);
+int str_compare(char *, char *);
+char *str_starts_with(const char *, const char *);
+char *str_concat(char *, char *);
 
 /* toem_strn_len1.c */
-char *toem_strcpy(char *, char *);
-char *toem_strdup(const char *);
-void toem_puts(char *);
-int toem_putchar(char);
+char *str_copy(char *, char *);
+char *str_duplicate(const char *);
+void print_string(char *);
+int write_character(char);
 
 /* toem_split_strn.c */
-char **toem_strtow(char *, char *);
-char **toem_strtow2(char *, char);
+char **split_string(char *, char *);
+char **split_string2(char *, char *);
 
 /* toem_char_handler.c */
-int toem_replace_alias(info_t *);
-int toem_replace_vars(info_t *);
-int toem_replace_string(char **, char *);
+int is_chain(info_t *, char *, size_t *);
+void check_chain_continue(info_t *, char *, size_t *, size_t, size_t);
+int replace_alias(info_t *);
+int replace_vars(info_t *);
+int replace_string(char **, char *);
 
 #endif
+
 
 
