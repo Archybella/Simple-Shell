73f5f901504f001f400130179f6f10b1dfebcb13
OukoRue <daisyouko114@gmail.com>
2023-07-24 02:28:25 -0700

    errors1

--- psb_errors.c
+++ psb_errors.c
@@ -1,26 +1,26 @@
 #include "shell.h"
 
 /**
- * _eputs - displays contents of a given string as output
+* _eputs - displays contents of a given string as output
 * @str: string to display
 *
 * Return: No explicit return value
 */
 void _eputs(char *str)
 {
-	int i = 0;
+int i = 0;
 
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_eputchar(str[i]);
-		i++;
-	}
+if (!str)
+return;
+while (str[i] != '\0')
+{
+_eputchar(str[i]);
+i++;
+}
 }
 
 /**
- * _eputchar - prints the single character(c) output to the stderr
+* _eputchar - prints the single character(c) output to the stderr
 * @c: ouput character
 *
 * Return: 1 if the output is a success, otherwise -1 is returned and
@@ -28,21 +28,21 @@ void _eputs(char *str)
 */
 int _eputchar(char c)
 {
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
+static int i;
+static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(2, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+write(2, buf, i);
+i = 0;
+}
+if (c != BUF_FLUSH)
+buf[i++] = c;
+return (1);
 }
 
 /**
- * _putfd - writes a single  character(c) to specified file descriptor(fd)
+* _putfd - writes a single  character(c) to specified file descriptor(fd)
 * @c: displayed character
 * @fd: file descriptor to be printed
 *
@@ -51,21 +51,21 @@ int _eputchar(char c)
 */
 int _putfd(char c, int fd)
 {
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
+static int i;
+static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(fd, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+write(fd, buf, i);
+i = 0;
+}
+if (c != BUF_FLUSH)
+buf[i++] = c;
+return (1);
 }
 
 /**
- *_putsfd - Outputs an input string to a specified fd
+*_putsfd - Outputs an input string to a specified fd
 * @str: string output
 * @fd: file descriptor to be printed
 *
@@ -73,13 +73,13 @@ int _putfd(char c, int fd)
 */
 int _putsfd(char *str, int fd)
 {
-	int i = 0;
+int i = 0;
 
-	if (!str)
-		return (0);
-	while (*str)
-	{
-		i += _putfd(*str++, fd);
-	}
-	return (i);
+if (!str)
+return (0);
+while (*str)
+{
+i += _putfd(*str++, fd);
+}
+return (i);
 }
