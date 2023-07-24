a36d2496e99ea903a9cd24e8609e35caf0090a26
OukoRue <daisyouko114@gmail.com>
2023-07-24 02:56:29 -0700

    errors

--- psb_errors1.c
+++ psb_errors1.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _erratoi - encodes a string as an integer represenation
+* _erratoi - encodes a string as an integer represenation
 * @s: sequence to be transfered
 * Return: The converted number if the string contains valid numbers,
 * 0 if the string does not contain any numbers,
@@ -9,28 +9,28 @@
 */
 int _erratoi(char *s)
 {
-	int i = 0;
-	unsigned long int result = 0;
+int i = 0;
+unsigned long int result = 0;
 
-	if (*s == '+')
-		s++;  /* TODO: why does this make main return 255? */
-	for (i = 0;  s[i] != '\0'; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-			if (result > INT_MAX)
-				return (-1);
-		}
-		else
-			return (-1);
-	}
-	return (result);
+if (*s == '+')
+s++;  /* TODO: why does this make main return 255? */
+for (i = 0;  s[i] != '\0'; i++)
+{
+if (s[i] >= '0' && s[i] <= '9')
+{
+result *= 10;
+result += (s[i] - '0');
+if (result > INT_MAX)
+return (-1);
+}
+else
+return (-1);
+}
+return (result);
 }
 
 /**
- * print_error - displays an error output
+* print_error - displays an error output
 * @info: pointer to the input, it returns the data structure
 * @estr: string that specifies the type of error
 *
@@ -39,13 +39,13 @@ int _erratoi(char *s)
 */
 void print_error(info_t *info, char *estr)
 {
-	_eputs(info->fname);
-	_eputs(": ");
-	print_d(info->line_count, STDERR_FILENO);
-	_eputs(": ");
-	_eputs(info->argv[0]);
-	_eputs(": ");
-	_eputs(estr);
+_eputs(info->fname);
+_eputs(": ");
+print_d(info->line_count, STDERR_FILENO);
+_eputs(": ");
+_eputs(info->argv[0]);
+_eputs(": ");
+_eputs(estr);
 }
 
 /**
@@ -57,38 +57,38 @@ void print_error(info_t *info, char *estr)
 */
 int print_d(int input, int fd)
 {
-	int (*__putchar)(char) = _putchar;
-	int i, count = 0;
-	unsigned int _abs_, current;
+int (*__putchar)(char) = _putchar;
+int i, count = 0;
+unsigned int _abs_, current;
 
-	if (fd == STDERR_FILENO)
-		__putchar = _eputchar;
-	if (input < 0)
-	{
-		_abs_ = -input;
-		__putchar('-');
-		count++;
-	}
-	else
-		_abs_ = input;
-	current = _abs_;
-	for (i = 1000000000; i > 1; i /= 10)
-	{
-		if (_abs_ / i)
-		{
-			__putchar('0' + current / i);
-			count++;
-		}
-		current %= i;
-	}
-	__putchar('0' + current);
-	count++;
+if (fd == STDERR_FILENO)
+__putchar = _eputchar;
+if (input < 0)
+{
+_abs_ = -input;
+__putchar('-');
+count++;
+}
+else
+_abs_ = input;
+current = _abs_;
+for (i = 1000000000; i > 1; i /= 10)
+{
+if (_abs_ / i)
+{
+__putchar('0' + current / i);
+count++;
+}
+current %= i;
+}
+__putchar('0' + current);
+count++;
 
-	return (count);
+return (count);
 }
 
 /**
- * convert_number - conversion utility similar to itoa
+* convert_number - conversion utility similar to itoa
 * @num: count
 * @base: radix for conversion
 * @flags: flags specifying additional conversion options
@@ -97,44 +97,44 @@ int print_d(int input, int fd)
 */
 char *convert_number(long int num, int base, int flags)
 {
-	static char *array;
-	static char buffer[50];
-	char sign = 0;
-	char *ptr;
-	unsigned long n = num;
+static char *array;
+static char buffer[50];
+char sign = 0;
+char *ptr;
+unsigned long n = num;
 
-	if (!(flags & CONVERT_UNSIGNED) && num < 0)
-	{
-		n = -num;
-		sign = '-';
-	}
-	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
-	ptr = &buffer[49];
-	*ptr = '\0';
+if (!(flags & CONVERT_UNSIGNED) && num < 0)
+{
+n = -num;
+sign = '-';
+}
+array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+ptr = &buffer[49];
+*ptr = '\0';
 
-	do	{
-		*--ptr = array[n % base];
-		n /= base;
-		} while (n != 0);
+do	{
+*--ptr = array[n % base];
+n /= base;
+} while (n != 0);
 
-	if (sign)
-		*--ptr = sign;
-	return (ptr);
+if (sign)
+*--ptr = sign;
+return (ptr);
 }
 
 /**
- * remove_comments - replaces the first occurrence of '#' with null char '\0
+* remove_comments - replaces the first occurrence of '#' with null char '\0
 * @buf: points to the sequence to be modified
 * Return: Consistently 0
 */
 void remove_comments(char *buf)
 {
-	int i;
+int i;
 
-	for (i = 0; buf[i] != '\0'; i++)
-		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
-		{
-			buf[i] = '\0';
-			break;
-		}
+for (i = 0; buf[i] != '\0'; i++)
+if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+{
+buf[i] = '\0';
+break;
+}
 }
